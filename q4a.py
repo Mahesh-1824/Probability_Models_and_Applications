9df08dc6a36df2d15ce54e34ceb922692f97c789
Mahesh <maheshderangula053@gmail.com>
2023-11-06 19:08:06 +0530

    4a done

--- q4a.py
+++ q4a.py
@@ -9,12 +9,12 @@ probability_matrix = [[0,1/3,1/3,1/3,0,0,0,0],
                       [0,0,1/3,1/3,0,0,0,1/3],
                       [0,1/3,1/3,0,0,0,0,1/3],
                       [0,0,0,0,0,0,0,1]]
-B = np.ones(8)
-A = np.eye(8)
+sum_of_prob = np.ones(8)
+prob = np.eye(8)
 for i in range(8):
     for j in range(8):
         if probability_matrix[i][j] != 0 and j != 7:
-            A[i][j] = -probability_matrix[i][j]
-x = np.linalg.solve(A, B)
+            prob[i][j] = -probability_matrix[i][j]
+times = np.linalg.solve(prob, sum_of_prob)
 
-print("The number of steps taken is  " + str(math.ceil(x[0])))
+print("The number of steps taken is  " + str(math.ceil(times[0])))
