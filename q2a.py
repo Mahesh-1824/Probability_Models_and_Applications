85fb960540de9634decf47cbd0c7c6624fbdc050
Mahesh <maheshderangula053@gmail.com>
2023-11-06 17:21:27 +0530

    2a done

--- q2a.py
+++ q2a.py
@@ -1,5 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.stats import norm
+# import seaborn as sns
 mean = (0+1+3)/10
 variance = 3*mean
+std_deviation = np.sqrt(variance)
+values_clt = []
+num_of_samples = 10000
+samples = 400
+for i in range(num_of_samples):
+    inverse = (np.sum(np.random.uniform(0,1,400))-200)/(20/np.sqrt(12))
+    values_clt.append(inverse*std_deviation + mean)
+
+x1 = np.linspace(-5, 5, 1000)
+# pdf_values_inbuilt = (1 / (std_deviation * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x1 - mean) / std_deviation) ** 2)
+pdf_values_inbuilt = norm.pdf(x1, loc=mean, scale=std_deviation)
+cdf_values_inbuilt = norm.cdf(x1, loc=mean, scale=std_deviation)
+# x = np.linspace(0, max(values_clt), 1000)
+# pdf_values = (x/sigma ** 2)*(np.exp(- (x ** 2)/(2 * (sigma ** 2))))
+# cdf_values = 1 - np.exp(-x**2 / (2 * sigma**2))
+plt.figure(figsize=(12, 6))
+plt.subplot(1,2,1)
+plt.plot(x1, pdf_values_inbuilt, label="Gaussian Inbuilt PDF")
+plt.hist(values_clt,bins=50,density=True, label='Gaussian Generated PDF')
+# sns.distplot(values_clt,hist=False, label="Rayleigh Generated PDF ")
+plt.xlabel("Gaussian Variable (x)")
+plt.ylabel("PDF")
+plt.legend()
+plt.title("PDF of Gaussian Random Variable ")
+
+plt.subplot(1,2,2)
+plt.plot(x1, cdf_values_inbuilt, label="Gaussian Inbuilt CDF")
+plt.hist(values_clt,bins=50,cumulative=True,density=True,label='Gaussian Generated CDF')
+# sns.distplot(values_clt,hist=False,kde_kws={"cumulative": True}, label="Rayleigh Generated CDF ")
+plt.xlabel("Gaussian Variable (x)")
+plt.ylabel("CDF")
+plt.legend()
+plt.title("CDF of Gaussian Random Variable ")
+plt.show()
